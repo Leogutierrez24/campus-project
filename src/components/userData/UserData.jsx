bd1b4254064191405c7df3d8a8b9bd761672ee8d
Leogutierrez24 <leoalegutierrez97@gmail.com>
2022-09-13 16:15:07 -0300

    add firebase to the project

--- src/components/userData/UserData.jsx
+++ src/components/userData/UserData.jsx
@@ -1,7 +1,23 @@
-import { React } from "react";
+import { React, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "../firebase/firebaseConfig";
 
 const UserData = () => {
-    return(<div>perfil</div>);
+    useEffect(() => {
+        const getData = async () => {
+            const data = await getDocs(collection(db, 'users'));
+            data.forEach((documento)=>{
+                console.log(documento.data());
+            });
+        }
+
+        getData();
+    }, []);
+
+    return(
+        <div>
+            perfil
+        </div>);
 }
 
 export default UserData;
